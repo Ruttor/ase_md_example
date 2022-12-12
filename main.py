6136c375bf8e94cff05c5c75a62c3598e7b1fe5f
Ruttor <fabianfelden.ff@gmail.com>
2022-12-12 11:51:43 +0100

    added run methods

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-'''
-from zntrack import Node, zn
-import ase
-import ase_md.simulator as asemd
-import typing
-import matplotlib.pyplot as plt
 
-class GetAtoms(Node):
-    """Generate Atoms for Simulation"""
-
-    # Inputs:
-    size: int = zn.params()
-    # Outputs:
-    atoms: ase.Atoms = zn.outs()
-    # Function:
-    def run(self):
-        self.atoms = asemd.generate_atoms(self.size)
-
-if __name__ == "__main__":
-    cool_atoms = GetAtoms(size=3)
-    cool_atoms.write_graph()
-
-
-'''
 from zntrack import Node, zn
 import ase_md.simulator as asemd
 import typing
+import ase_md
+
 
 class GetAtoms(Node):
     """Atome abholen"""
@@ -34,18 +13,43 @@ class GetAtoms(Node):
     def run(self):
         self.atoms = asemd.generate_atoms(self.size)
 
+class RunMD(Node):
+    ''' Run Method '''
+    atoms: GetAtoms = zn.deps()
+    temperature: int = zn.params()
+    timestep: float = zn.params()
+    steps: int = zn.params()
+    dump_interval: int = zn.params()
+    
+    atoms_list = zn.outs()
+
+    def run(self):
+        self.atoms_list = asemd_run simulation (
+                atoms = self.atoms,
+                temperature = self.temperature,
+                timestep = self.timestep,
+                steps = self.steps,
+                dump_interval = self.dump_interval
+            )
+
+class ComputeRDF(Node):
+    atoms_list: RunMD = zn.deps()
+    rmax: float = zn.params()
+    nbins: int = zn.params()
+    elements: str = zn.params()
+    
+    rdf: dict = zn.outs()
+
+    def run(self):
+        self.rdf = asemd.compute_rdf(
+                atoms_list = self.atoms_list.atoms_list,
+                rmax = self.rmax,
+                nbins = self.nbins,
+                elements = self.elements
+            )
 
 x = GetAtoms(size = 2)
 x.write_graph()
 
-
-
-#atoms = ase_md.simulator.generate_atoms(size=2)
-
-#atoms_list = ase_md.simulator.run_simulation(atoms=atoms, temperature=300, timestep=1.0, dump_interval=5, steps=20)
-
-#rdf = ase_md.simulator.compute_rdf(atoms_list=atoms_list, rmax=1.0, nbins=50, elements="Cu")
-
-#print(rdf)
 
 
